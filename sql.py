801e82fe9e9fb0d4661528a0b97c31be07d8d932
BuddaBaker <prasithchilla@gmail.com>
2023-01-22 20:47:52 -0800

    commit

--- sql.py
+++ sql.py
@@ -8,23 +8,24 @@ class Student:
         self.age = 0
         self.favorite_cuisine = ""
         self.favorite_restaurant = ""
-        self.conn = sqlite3.connect('/home/prasithchilla/vscode/flask/volumes/students.db') # Connects to or creates the '/home/prasithchilla/vscode/flask/volumes/students.db' file
-        self.create_table() # Creates the students table
+        try:
+            self.conn = sqlite3.connect('/home/prasithchilla/vscode/flask/volumes/students.db')
+            self.create_table()
+        except sqlite3.Error as e:
+            print(f'Error: {e}')
+            print('Could not connect to the database, please make sure the file path is correct and the user has the necessary permissions')
 
     def create_table(self):
         c = self.conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS students
-                     (name text, classOf text, dob text, age int, favorite_cuisine text, favorite_restaurant text)''')
-        self.conn.commit()
+        try:
+            c.execute('''CREATE TABLE IF NOT EXISTS students
+                         (name text, classOf text, dob text, age int, favorite_cuisine text, favorite_restaurant text)''')
+            self.conn.commit()
+            print("Table created successfully")
+        except sqlite3.Error as e:
+            print(f'Error creating table: {e}')
         c.close()
 
     def get_user_input(self):
         self.name = input("What is your name? ")
         self.classOf = input("What is your class of? ")
-        dob_input = input("What is your date of birth (yyyy-mm-dd)? ")
-        self.dob = datetime.strptime(dob_input, "%Y-%m-%d")
-        self.age = self.calculate_age()
-        self.favorite_cuisine = input("What is your favorite cuisine? ")
-        self.favorite_restaurant = input("What is your favorite restaurant? ")
-
-    # rest of the code
